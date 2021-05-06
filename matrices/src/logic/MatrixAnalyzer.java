8f2e260bb4b29c756af987b5f2f1b4ee39de0232
Marcos Méndez <marcosmh0199@gmail.com>
2021-05-06 01:22:55 -0600

    feat: check identity matrix

--- matrices/src/logic/MatrixAnalyzer.java
+++ matrices/src/logic/MatrixAnalyzer.java
@@ -1,5 +1,4 @@
 package logic;
-
 import Model.Matrix;
 
 public class MatrixAnalyzer {
@@ -15,6 +14,22 @@ public class MatrixAnalyzer {
   }
 
   public boolean isIdentity(Matrix m){
+    int height  = m.getHeight();
+    int width   = m.getWidth();
+    if(height != width){
+      return false;
+    }
+    for(int j = 0; j < height; j++){
+      for(int i = 0; i < width; i++){
+        if(j == i && m.getValue(i, j) == 0){
+          return false;
+        }else{
+          if(m.getValue(i, j) == 1){
+            return false;
+          }
+        }
+      }
+    }
     return true;
   }
 }
