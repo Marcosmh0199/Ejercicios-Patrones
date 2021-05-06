1b3febb2aaf581c8c360490addd9ba30128fea8f
Marcos Méndez <marcosmh0199@gmail.com>
2021-05-06 15:05:47 -0600

    feat: controller

---
+++ descuentos/src/controller/Controller.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import model.FlashDiscount;
+import model.User;
+
+public class Controller {
+  public static ArrayList<User> users;
+  public static ArrayList<FlashDiscount> flashDiscounts;
+
+  public Controller(){
+    setUsers();
+    setFlashDiscounts();
+  }
+
+  public ArrayList<User> getUsers() {
+    return users;
+  }
+  private void setUsers() {
+    users = new ArrayList<User>();
+  }
+
+  public ArrayList<FlashDiscount> getFlashDiscounts() {
+    return flashDiscounts;
+  }
+  private void setFlashDiscounts() {
+    flashDiscounts = new ArrayList<FlashDiscount>();
+  }
+
+  public void addFlashDiscounts(FlashDiscount fd){
+    flashDiscounts.add(fd);
+  }
+
+  public void addUser(User u){
+    users.add(u);
+  }
+}
