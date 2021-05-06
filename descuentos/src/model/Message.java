5534c109ef91c5f7f0ecf47965ee4f0c8e5f7122
Marcos Méndez <marcosmh0199@gmail.com>
2021-05-06 13:46:25 -0600

    feat: Message

--- descuentos/src/model/Message.java
+++ descuentos/src/model/Message.java
@@ -1,5 +1,46 @@
 package model;
 
 public class Message {
-  private String tittle;
+  private String subject;
+  private String from;
+  private String to;
+  private String body;
+
+  public Message(String subject, String from, String to, String body){
+    setSubject(subject);
+    setFrom(from);
+    setTo(to);
+    setBody(body);
+  }
+
+
+  public String getBody() {
+    return body;
+  }
+  public void setBody(String body) {
+    this.body = body;
+  }
+
+  public String getTo() {
+    return to;
+  }
+  public void setTo(String to) {
+    this.to = to;
+  }
+
+  public String getFrom() {
+    return from;
+  }
+  public void setFrom(String from) {
+    this.from = from;
+  }
+
+  public String getSubject() {
+    return subject;
+  }
+  public void setSubject(String subject) {
+    this.subject = subject;
+  }
+
+  
 }
